eb5f4655c22e639e4266b9c88a1bf1c905f39a52
Kouki Shiroishi <k-shiroishi@f-com.co.jp>
2025-06-26 13:38:03 +0900

    サーバのIPアドレスとホスト名を返却するように修正

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php
 
-http_response_code(200);
+function getLocalAddress() {
+	return $_SERVER['SERVER_ADDR'] ?? null;
+}
 
-$array = array('test' => 'test',
-               'abc'  => 'test2');
-		   
+function getLocalHostName() {
+	if (is_null(getLocalAddress())) {
+		$hostname = null;
+	} else {
+		$hostname  = gethostbyaddr($server_ip);
+	}
+	return $hostname;
+}
+
+$server_ip = getLocalAddress();
+$hostname  = getLocalHostName();
+
+$array = array('ipaddress' => $server_ip,
+			   'hostname'  => $hostname);
+
 $json = json_encode($array);
 
+http_response_code(200);
+header("Content-Type: application/json");
 echo $json;
