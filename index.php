e6e4bb42108f17b3960674e9f6614c9f1ad208ab
Kouki Shiroishi <k-shiroishi@f-com.co.jp>
2025-06-27 19:00:29 +0900

    ソースコードを修正

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ function getLocalAddress() {
 }
 
 function getLocalHostName() {
-	if (is_null(getLocalAddress())) {
+	$server_ip = getLocalAddress()
+	if (is_null($server_ip)) {
 		$hostname = null;
 	} else {
 		$hostname  = gethostbyaddr($server_ip);
@@ -20,6 +21,7 @@ $array = array('ipaddress' => $server_ip,
 			   'hostname'  => $hostname);
 
 $json = json_encode($array);
+
 
 http_response_code(200);
 header("Content-Type: application/json");
